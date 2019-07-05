832e42d5855e1583d83b85ff24381f2b567d2e4d
DanielVeB <danielkurosz@gmail.com>
2019-07-05 12:27:14 +0200

    update DTO
    add date format for json
    add constructor without passing date

--- src/main/java/com/comarch/danielkurosz/dto/ClientDTO.java
+++ src/main/java/com/comarch/danielkurosz/dto/ClientDTO.java
@@ -1,20 +1,30 @@
 package com.comarch.danielkurosz.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
+
 
 public class ClientDTO {
 
     private String firstName;
     private String lastName;
     private String email;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm a z")
     private Date creationDate;
 
+    public ClientDTO(){}
 
     public ClientDTO(String email, String firstName, String lastName, Date creationDate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.creationDate = creationDate;
+    }
+    public ClientDTO(String email, String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
     public String getFirstName() {
