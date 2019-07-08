ba7aaa27bd6abc57035258ebb9779b1e716cdf5a
DanielVeB <danielkurosz@gmail.com>
2019-07-08 16:01:28 +0200

    connect with mongoDB with morphia

--- src/main/java/com/comarch/danielkurosz/dao/MongoClientDAO.java
+++ src/main/java/com/comarch/danielkurosz/dao/MongoClientDAO.java
@@ -1,29 +1,29 @@
 package com.comarch.danielkurosz.dao;
 
 import com.comarch.danielkurosz.data.ClientEntity;
+import com.mongodb.DuplicateKeyException;
+import com.mongodb.MongoClient;
+import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.List;
 
 public class MongoClientDAO implements ClientDAO {
 
-    LinkedList<ClientEntity> clientsEntities;
-
     final Morphia morphia = new Morphia();
+    private Datastore datastore;
 
     public MongoClientDAO() {
+        this.morphia.mapPackage("com.comarch.danielkurosz.data");
+        this.datastore = morphia.createDatastore(new MongoClient(), "dropwizard");
 
-        clientsEntities = new LinkedList<>();
-        clientsEntities.push(new ClientEntity("daniel@", "Daniel", "Kurosz",new Date()));
-        clientsEntities.push(new ClientEntity("tomek@", "Tomasz", "Kowalski",new Date()));
-        clientsEntities.push(new ClientEntity("janek@", "Jan", "Nowak",new Date()));
-        clientsEntities.push(new ClientEntity("jan@", "Jan", "Kowal",new Date()));
     }
 
     @Override
-    public boolean create(ClientEntity clientEntity) {
-        clientsEntities.push(clientEntity);
+    public boolean create(ClientEntity clientEntity) throws DuplicateKeyException {
+        datastore.save(clientEntity);
         return true;
     }
 
@@ -34,28 +34,34 @@ public class MongoClientDAO implements ClientDAO {
 
     @Override
     public void delete(ClientEntity clientEntity) {
-        clientsEntities.remove(clientEntity);
+
     }
 
     @Override
     public LinkedList<ClientEntity> getByName(String name) {
         LinkedList<ClientEntity> returnClientEntities = new LinkedList<>();
-        for (ClientEntity c : clientsEntities) {
-            if (c.getFirstName().equals(name)) returnClientEntities.push(c);
-        }
+
         return returnClientEntities;
     }
 
     @Override
     public ClientEntity getByEmail(String email) {
-        for (ClientEntity c : clientsEntities) {
-            if (c.getEmail().equals(email)) return c;
-        }
+
         return null;
     }
 
     @Override
-    public LinkedList<ClientEntity> getAll() {
-        return clientsEntities;
+    public List<ClientEntity> getAll() {
+
+        List<ClientEntity> clientEntities= new LinkedList<>();
+
+        try {
+            clientEntities = this.datastore.find(ClientEntity.class).asList();
+
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        return clientEntities;
     }
 }
